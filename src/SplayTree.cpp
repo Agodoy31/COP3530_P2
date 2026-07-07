ee7dd960bc5719664eccf29e73c8ab4da3486fbf
Agodoy31 <casata177@gmail.com>
2026-07-06 20:22:27 -0400

    feat: implement Splay Tree rotations and corresponding Catch2 unit tests

--- src/SplayTree.cpp
+++ src/SplayTree.cpp
@@ -1,1 +1,67 @@
-// SplayTree.cpp
+#include "SplayTree.h"
+
+SplayTree::SplayTree() {
+    root = nullptr;
+    totalNodes = 0;
+    nodesTraversed = 0;
+}
+
+int SplayTree::getNodesTraversed() {
+    return nodesTraversed;
+}
+
+void SplayTree::resetNodesTraversed() {
+    nodesTraversed = 0;
+}
+
+size_t SplayTree::getMemoryUsageInBytes() {
+    return totalNodes * sizeof(SplayNode);
+}
+
+void SplayTree::leftRotate(SplayNode* x) {
+    SplayNode* y = x->right;
+    if (y == nullptr) return;
+
+    x->right = y->left;
+    if (y->left != nullptr) {
+        y->left->parent = x;
+    }
+
+    y->parent = x->parent;
+    if (x->parent == nullptr) {
+        root = y;
+    }
+    else if (x == x->parent->left) {
+        x->parent->left = y;
+    }
+    else {
+        x->parent->right = y;
+    }
+
+    y->left = x;
+    x->parent = y;
+}
+
+void SplayTree::rightRotate(SplayNode* x) {
+    SplayNode* y = x->left;
+    if (y == nullptr) return;
+
+    x->left = y->right;
+    if (y->right != nullptr) {
+        y->right->parent = x;
+    }
+
+    y->parent = x->parent;
+    if (x->parent == nullptr) {
+        root = y;
+    }
+    else if (x == x->parent->right) {
+        x->parent->right = y;
+    }
+    else {
+        x->parent->left = y;
+    }
+
+    y->right = x;
+    x->parent = y;
+}
--- src/SplayTree.h
+++ src/SplayTree.h
@@ -1,1 +1,45 @@
-// SplayTree.h
+#pragma once
+#include <string>
+
+struct SplayNode {
+    int patientID;
+    short age;
+    char gender;
+    std::string bloodType;
+    std::string condition;
+    std::string dateOfAdmission;
+    std::string doctor;
+    std::string hospital;
+    std::string insuranceProvider;
+    double billingAmount;
+
+    SplayNode* left = nullptr;
+    SplayNode* right = nullptr;
+    SplayNode* parent = nullptr;
+
+    SplayNode(int id, short a, char g, std::string bType, std::string cond,
+        std::string date, std::string doc, std::string hosp,
+        std::string ins, double bill)
+        : patientID(id), age(a), gender(g), bloodType(std::move(bType)),
+        condition(std::move(cond)), dateOfAdmission(std::move(date)),
+        doctor(std::move(doc)), hospital(std::move(hosp)),
+        insuranceProvider(std::move(ins)), billingAmount(bill) {}
+};
+
+class SplayTree {
+// private:
+public:
+    SplayNode* root;
+    int totalNodes;
+    int nodesTraversed;
+
+    void leftRotate(SplayNode* x);
+    void rightRotate(SplayNode* x);
+
+// public:
+    SplayTree();
+
+    int getNodesTraversed();
+    void resetNodesTraversed();
+    size_t getMemoryUsageInBytes();
+};
