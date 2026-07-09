8f303b2424da0a625c974b0ed67d2f578072729b
Sai <saraj62577@gmail.com>
2026-07-09 14:48:52 -0400

    Implemented skip list insertion and search fuctions

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,19 @@
 // main.cpp
+#include "../src/SkipList.h"
 
 int main() {
+
+    SkipList list;
+    list.insert({1001, 49, "Aetna", "Dr. Smith", "O+"});
+    list.insert({1002, 48, "Cigna", "Dr. Lee", "A-"});
+    list.insert({1003, 47, "Humana", "Dr. Patel", "B+"});
+    list.insert({1003, 49, "Humana", "Dr. Patel", "B+"});
+    list.display_list();
+
+
+
+
+
+
     return 0;
 }
